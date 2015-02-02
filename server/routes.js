6138c906e7b4e7401a7727a337574a795daa6b07
Icefla <iceflasheroid@gmail.com>
2015-02-02 09:05:27 +0000

    feature : запрос getFriends

--- server/routes.js
+++ server/routes.js
@@ -442,7 +442,7 @@ function userlist(req, res, next) {
         res.status(status).json(userlist);
     });
   }else{
-    userModel.getFriends(userId, function(status, message, userlist) {
+    userModel.getFriends(userId, Number(start), Number(count), function(status, message, userlist) {
       if(status === errors.restStat_isOk)
         res.status(status).json(userlist);
         else console.log(message);
